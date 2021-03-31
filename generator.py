df8b563d925e8599cf8b1a30f5a0bec2b9973e27
JorisJoriukas <67592193+JorisJoriukas@users.noreply.github.com>
2021-03-31 11:19:13 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,12 @@
+name = ['apples', 'bananas', 'oranges', 'cats', 'dogs']
+verb = ['blue', 'green', 'red']
+noun = ['261', '461', '669']
+from random import randint
+def pick(words):
+    num_words = len(words)
+    num_picked = randint(0, num_words - 1)
+    word_picked = words[num_picked]
+    return word_picked
+while True:
+     print(pick(verb), pick(name), 'a', pick(noun), end='.')
+     input()
