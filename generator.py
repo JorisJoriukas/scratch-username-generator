9bc08d3943463935e33b6b0a1366a04cc96cd7e1
JorisJoriukas <67592193+JorisJoriukas@users.noreply.github.com>
2021-04-04 18:12:11 +0300

    Update generator.py
    Here's the improved code.

--- generator.py
+++ generator.py
@@ -1,13 +1,14 @@
+from random import randint
 name = ['apples', 'bananas', 'oranges', 'cats', 'dogs']
 verb = ['blue', 'green', 'red']
-noun = ['261', '461', '669']
-from random import randint
+noun = [randint(1, 999)]
 def pick(words):
     num_words = len(words)
     num_picked = randint(0, num_words - 1)
     word_picked = words[num_picked]
     return word_picked
 while True:
-     print(pick(verb), pick(name), pick(noun), end='.')
-     print('Heres your username.')
+     print(pick(verb), pick(name), pick(noun))
+     print('Here\'s your username.')
      input()
+    
